4bd40902243753bb30a95682fc0888e6736be39e
roman16 <roman.kostyanko@gmail.com>
2025-12-29 11:46:22 +0200

    Refactor registration and login pages to simplify container class names and update global styles for improved layout consistency and responsiveness.

--- src/app/(auth)/registration/page.tsx
+++ src/app/(auth)/registration/page.tsx
@@ -3,7 +3,7 @@ import {AuthDetails} from "@/app/(auth)/login/AuthDetails";
 
 export default function RegistrationPage() {
     return (
-        <div className="page auth-page registration-page">
+        <div className="auth-page registration-page">
             <div className={'auth-page__form-container'}>
                 <h2>Вітаємо у GarageOS</h2>
                 <p>Платформа для тих, хто цінує порядок та швидкість</p>
